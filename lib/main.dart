f85e22bf05b4d99eed76eb6c7bb437924f2e43db
Zubisofts <zubitex40@gmail.com>
2021-05-26 14:06:27 +0100

    Added Screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class _MyAppState extends State<MyApp> {
             debugShowCheckedModeBanner: false,
             darkTheme: AppTheme.darkTheme,
             theme: AppTheme.lightTheme,
-            themeMode: isDarkTheme ? ThemeMode.dark : ThemeMode.light,
+            themeMode: ThemeMode.dark,
             home: App(),
           );
         },
